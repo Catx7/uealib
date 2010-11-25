2e4ad35bf4b53c0b51d81e00288cc069d3ecc1b5
Loginov.Nick <Loginov.Nick@5b08095d-5c25-ebdf-e74a-48b85d74865c>
2010-11-25 13:27:15 +0000

    Реализация основных классов Simulated Annealing

--- trunk/uealib/src/simulatedannealing/MetropolisRule.java
+++ trunk/uealib/src/simulatedannealing/MetropolisRule.java
@@ -1,12 +1,34 @@
 package simulatedannealing;
 
 import core.Generation;
+import core.Solution;
+
+/** 
+ * Стандартное правило перехода для Simulated Annealing
+ *
+ */
 
 public class MetropolisRule implements core.TransitionCriteria {
 
 	@Override
 	public boolean isSatisfied(Generation g, Generation h) {
-		// TODO Auto-generated method stub
+		SimulatedAnnealingContext ctx = SimulatedAnnealingContext.getInstance();		
+		double t = ctx.getShedule().getTemperature();
+		
+		Solution current = (Solution)g.get(0);
+		Solution next = (Solution)h.get(0);
+		
+		if(current.compareTo(next) < 0) {
+			return true;
+		}
+		
+		double delta = Math.abs(current.getFitness()-next.getFitness());
+		
+		
+		if(Math.random() < Math.exp(-delta/t)) {
+			return true;
+		}
+		
 		return false;
 	}
 
